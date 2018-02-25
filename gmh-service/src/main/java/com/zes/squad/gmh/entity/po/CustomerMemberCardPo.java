858ccee49209fda94ef4be37e04bd9f60b77a8c9
zpf <zpf@DESKTOP-UJBRQNC>
2018-02-26 00:47:14 +0800

    customerMemberCard

---
+++ gmh-service/src/main/java/com/zes/squad/gmh/entity/po/CustomerMemberCardPo.java
@@ -0,0 +1,31 @@
+package com.zes.squad.gmh.entity.po;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class CustomerMemberCardPo extends Po{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long customerId;
+	private Long memberCardId;
+	private BigDecimal remainingMoney;
+	private Integer remainingTimes;
+	private Integer isValid;
+	private Integer isReturned;
+	private String returnedReason;
+	private Date returnedTime;
+	private BigDecimal returnedMoney;
+	private Integer isTurned;
+	private String turnedReason;
+	private BigDecimal turnedMoney;
+	private String uniqueIdentifier;
+	private Long storeId;
+}

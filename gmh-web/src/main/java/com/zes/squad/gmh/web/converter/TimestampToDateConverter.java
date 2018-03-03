0e967bec8a9b611dfddd524ed94993d94c1c39de
yuhuan.zhou <yuhuan.zhou@192.168.0.108>
2018-03-03 14:29:29 +0800

    修改web层converter类名

--- gmh-web/src/main/java/com/zes/squad/gmh/web/converter/TimestampToDateConverter.java
+++ gmh-web/src/main/java/com/zes/squad/gmh/web/converter/TimestampToDateConverter.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 import org.springframework.core.convert.converter.Converter;
 
-public class LongToDateConverter implements Converter<Long, Date> {
+public class TimestampToDateConverter implements Converter<Long, Date> {
 
     @Override
     public Date convert(Long source) {

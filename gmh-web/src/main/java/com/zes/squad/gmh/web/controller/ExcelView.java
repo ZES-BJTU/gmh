babfa7873471257eddaba2703071fe9bf26510b6
yuhuan.zhou <yuhuan.zhou@DESKTOP-HUNPPFM>
2018-03-27 00:43:06 +0800

    添加excel生成模板

---
+++ gmh-web/src/main/java/com/zes/squad/gmh/web/controller/ExcelView.java
@@ -0,0 +1,33 @@
+package com.zes.squad.gmh.web.controller;
+
+import java.io.OutputStream;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.servlet.view.document.AbstractXlsxStreamingView;
+
+import lombok.Cleanup;
+
+public class ExcelView extends AbstractXlsxStreamingView {
+
+    private static final String DEFAULT_EXCEL_CONTENT_TYPE = "application/vnd.ms-excel;charset=UTF-8";
+
+    @Override
+    public void buildExcelDocument(Map<String, Object> model, Workbook workbook, HttpServletRequest request,
+                                   HttpServletResponse response)
+            throws Exception {
+        String fileName = (String) model.get("fileName");
+        response.setHeader("Content-Disposition",
+                "attachment;filename=" + new String(fileName.getBytes("UTF-8"), "ISO-8859-1"));
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType(DEFAULT_EXCEL_CONTENT_TYPE);
+        @Cleanup
+        OutputStream outputStream = response.getOutputStream();
+        workbook.write(outputStream);
+        outputStream.flush();
+    }
+
+}
--- gmh-web/src/main/java/com/zes/squad/gmh/web/controller/ExportController.java
+++ gmh-web/src/main/java/com/zes/squad/gmh/web/controller/ExportController.java
@@ -1,8 +1,28 @@
 package com.zes.squad.gmh.web.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping(path = "/export")
+@Controller
 public class ExportController {
+
+    @RequestMapping(path = "/stocks")
+    public ModelAndView doExportStock(ModelMap map, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        map.put("fileName", "库存.xlsx");
+        Workbook workbook = new XSSFWorkbook();
+        workbook.createSheet();
+        ExcelView excelView = new ExcelView();
+        excelView.buildExcelDocument(map, workbook, request, response);
+        return new ModelAndView(excelView, map);
+    }
 
 }

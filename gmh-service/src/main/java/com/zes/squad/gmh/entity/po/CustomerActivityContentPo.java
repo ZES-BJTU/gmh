3484e5f506df09668b90c9267c733f85385058d7
yuhuan.zhou <yuhuan.zhou@DESKTOP-HUNPPFM>
2018-04-01 00:38:48 +0800

    修改单词拼写错误

--- gmh-service/src/main/java/com/zes/squad/gmh/entity/po/CustomerActivityContentPo.java
+++ gmh-service/src/main/java/com/zes/squad/gmh/entity/po/CustomerActivityContentPo.java
@@ -5,17 +5,19 @@ import java.math.BigDecimal;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class CustomerActivityContentPo extends Po{
+public class CustomerActivityContentPo extends Po {
 
-	private static final long serialVersionUID = 1L;
-	
-	private Long customerActivityId;
-	private Integer type;
-	private Long relatedId;
-	private String content;
-	private BigDecimal number;
-	private String remark;
+    private static final long serialVersionUID = 1L;
+
+    private Long              customerActivityId;
+    private Integer           type;
+    private Long              relatedId;
+    private String            content;
+    private BigDecimal        number;
+    private String            remark;
 }

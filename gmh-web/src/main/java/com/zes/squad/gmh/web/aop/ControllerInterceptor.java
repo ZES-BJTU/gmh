309d3e82751134d9628ef37370b26a0df04eeb0e
yuhuan.zhou <yuhuan.zhou@DESKTOP-HUNPPFM>
2018-04-18 00:33:08 +0800

    controller添加时间监控

---
+++ gmh-web/src/main/java/com/zes/squad/gmh/web/aop/ControllerInterceptor.java
@@ -0,0 +1,46 @@
+package com.zes.squad.gmh.web.aop;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+import com.zes.squad.gmh.common.exception.ErrorCodeEnum;
+import com.zes.squad.gmh.common.exception.GmhException;
+import com.zes.squad.gmh.common.util.JsonUtils;
+import com.zes.squad.gmh.web.common.JsonResults;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ControllerInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        long beginTime = System.currentTimeMillis();
+        try {
+            Object result = invocation.proceed();
+            long endTime = System.currentTimeMillis();
+            log.error("调用controller成功, controller is {}, params is {}, 接口耗时:{}ms", getApiInfo(invocation),
+                    JsonUtils.toJson(invocation.getArguments()), endTime - beginTime);
+            return result;
+        } catch (GmhException e) {
+            long endTime = System.currentTimeMillis();
+            log.error("调用controller异常, controller is {}, params is {}, 接口耗时:{}ms", getApiInfo(invocation),
+                    JsonUtils.toJson(invocation.getArguments()), endTime - beginTime, e);
+            return JsonResults.fail(e.getCode(), e.getMessage());
+        } catch (Exception e) {
+            long endTime = System.currentTimeMillis();
+            log.error("调用controller异常, controller is {}, params is {}, 接口耗时:{}ms", getApiInfo(invocation),
+                    JsonUtils.toJson(invocation.getArguments()), endTime - beginTime, e);
+            return JsonResults.fail(ErrorCodeEnum.UNKNOWN_EXCEPTION.getCode(), "服务器开小差了");
+        }
+    }
+
+    private String getApiInfo(MethodInvocation invocation) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(invocation.getThis().getClass().getName());
+        builder.append("#");
+        builder.append(invocation.getMethod().getName());
+        return builder.toString();
+    }
+
+}
--- gmh-web/src/main/java/com/zes/squad/gmh/web/exception/handler/ControllerExceptionHandler.java
+++ gmh-web/src/main/java/com/zes/squad/gmh/web/exception/handler/ControllerExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.zes.squad.gmh.web.exception.handler;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.zes.squad.gmh.common.exception.ErrorCodeEnum;
 import com.zes.squad.gmh.common.exception.GmhException;
@@ -10,8 +9,8 @@ import com.zes.squad.gmh.web.common.JsonResults.JsonResult;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Deprecated
 @Slf4j
-@RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler({ GmhException.class })
